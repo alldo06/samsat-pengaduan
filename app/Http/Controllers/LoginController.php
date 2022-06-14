efeb143c8ad568f73137b257629071d5811ef972
alldo06 <aldovector06@gmail.com>
2022-06-14 12:16:21 +0700

    fix ui alert

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
 			return redirect()->intended('/dashboard');
 		}
 
-		return back()->with('loginError', 'Login Failed !');
+		return back()->with('loginError', 'Login Failed');
 	}
 
 	public function logout(Request $request)
