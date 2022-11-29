eeab6c5d968f4f2ba84d475e5bb19b64da7cbad4
alldo06 <aldovector06@gmail.com>
2022-11-29 10:25:25 +0700

    fix some css

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="global-footer">
-	<p>P3D Wilayah Kab. Indramayu II Haurgeulis</p>
+	<p>2022 &#169; P3D Wilayah Kab. Indramayu II Haurgeulis</p>
 	<p class="author"><span>by</span> RG</p>
 </footer>
